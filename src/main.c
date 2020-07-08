2b2c2c6658c824c6cf7bb8786677d892b8b1c930
lukas-sr <lukas-sr@hotmail.com>
2020-07-08 16:17:40 -0300

    will desapear

--- src/main.c
+++ src/main.c
@@ -1,17 +1,11 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+   int *p = (unsigned int*) malloc(sizeof(int));
+   
+   
 
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+   printf("%d", *p);
+   return 0;
 }
