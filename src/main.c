f98090363f4bf040993715d50945a0f8593b5fd0
lukas-sr <lukas-sr@hotmail.com>
2020-07-13 21:13:30 -0300

    right implementation with 4 numbers

--- src/main.c
+++ src/main.c
@@ -1,63 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 #define MAX 32
-#define MAX_THREADS 4
+#define MAX_PROCS 4
 
-int i = 0, n_primos = 0, threads = 0, size_vet = 0;
-unsigned int vetor[MAX];
+unsigned int vetor[MAX], size_vet = 0;
 
 //input de valores para o vetor
-void catch_values(){
+void input_vetor(){
+   int i;
    char unit = '\0';
-   
-   //for ( i = 0 ; i < MAX ; i++) vetor[i] = '\0';
-    
+
 	for ( i = 0 ; i < MAX ; i++){
-      unit = scanf("%u", &vetor[i]); 
-      
+      unit = scanf("%u", &vetor[i]);
+
       if( unit == EOF ){
-			size_vet = i; //SÓ PARA DEBUG <===
-         break;
-		}
+			     size_vet = i;
+           break;
+		  }
    }
 }
 
-void analyze(int number){
-   pid_t pid_local;
-   int j, cont_div;
-   
-   pid_local = fork();
-   
-   if (pid_local == 0){
-      
-      for( j = 0 ; j < number ; j++){
-         if (number % j == 0) cont_div++;
+//verifica se é primo
+unsigned int verifica_primo(int number){
+   int cont_div = 0, n_primos = 0;
 
-         if (cont_div++ >= 2) break;
-      }
-      
-      if (cont_div == 1) n_primos++;
+   for(int j = 1; j <= number ; j++){
+     //se é um divisor e o numero é diferente de 1, conta +1 divisor
+     if ((number % j == 0) && (j != 1)) cont_div++;
 
-      exit(0);
+     //se encontrar um segundo divisor pode quebrar o laço
+     if (cont_div > 1) break;
    }
-   
 
+  //se só houver 1 divisor, o número é primo
+  if (cont_div == 1) n_primos++;
+
+  return n_primos;
 }
 
 int main(){
-   system("clear");
-   catch_values();
+   pid_t PID[MAX_PROCS];
 
-   //SÓ PARA DEBUG <==
-   for (i = 0 ; i < size_vet ; i++) printf("%d ", vetor[i]);
+   //definição de flags de proteção e visibilidade da memória
+   int protection = PROT_READ | PROT_WRITE;
+   int visibility = MAP_SHARED| MAP_ANON;
+   //area de memória compartilhada
+   int *shrd_memory, primos;
+   shrd_memory = (int *) mmap(NULL, sizeof(int)*MAX, protection, visibility, 0, 0);
+   (*shrd_memory) = 0;
 
-   for (i = 0 ; i < size_vet ; i++){
-      if(threads <= MAX_THREADS) analyze(vetor[i]);
-      
+   //lê valores do usuario
+   input_vetor();
+
+   for (int i = 0; i < MAX_PROCS ; i++){
+      PID[i] = fork();
+      //se for um processo filho
+      if (PID[i] == 0){
+          primos = verifica_primo(vetor[i]);
+          *shrd_memory += primos;
+          exit(0);
+      }
+      printf("Process filho %d com num_primos = %d\n", PID[i], *shrd_memory);
    }
 
-	return 0;
+   for (int k = 0 ; k < MAX_PROCS; k++) waitpid(PID[k], NULL, 0);
+
+   printf("num_primos = %d\n", *shrd_memory);
+
+   //printf("%ls\n", shrd_memory);
+
+   return 0;
 }
